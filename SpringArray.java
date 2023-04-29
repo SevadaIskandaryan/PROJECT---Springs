8f35a1554a17f37b1d16e27eb0c0d636a0d385a7
Sevada <sevadaiskandaryan@gmail.com>
2023-04-29 22:36:25 +0400

    T2_2

--- SpringArray.java
+++ SpringArray.java
@@ -40,6 +40,51 @@ public class SpringArray {
         return new Spring(result);
     }
 
+    public Spring equivalentSpring(String springExpr, Spring[] springs){
+        Stack<Double> stack_spr_stiffness = new Stack<>();
+        for(int i = 0; i < springs.length; i++){
+            stack_spr_stiffness.push(springs[i].getSpring());
+        }
+        Stack<Double> stack = new Stack<>();
+        for (char c : springExpr.toCharArray()) {
+            System.out.println(c);
+            if (c == '{') {
+                stack.push(-1.0);
+            } else if (c == '[') {
+                stack.push(-2.0);
+            } else if (c == '}') {
+                double sum = 0;
+                while (stack.peek() != -1.0) {
+                    sum += 1/(stack.pop());
+                }
+                stack.pop();
+                if(sum == 0){
+                    double stiff = stack_spr_stiffness.pop();
+                    stack.push(stiff);
+                } else{
+                    stack.push(1/sum);
+                }
+            } else if (c == ']') {
+                double sum = 0;
+                while (stack.peek() != -2.0) {
+                    sum += stack.pop();
+                }
+                stack.pop();
+                if(sum == 0){
+                    double stiff = stack_spr_stiffness.pop();
+                    stack.push(stiff);
+                } else{
+                    stack.push(sum);
+                }
+            }
+        }
+
+        double result = stack.pop();
+        //return result
+        return new Spring(result);
+    }
+
+
     public static void main(String[] args) {
         //for testing
         //SpringArray s = new SpringArray();
