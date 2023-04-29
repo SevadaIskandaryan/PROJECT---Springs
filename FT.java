360f2d0fdcd9732c73652f49a463bc6fca6171ca
Sevada <sevadaiskandaryan@gmail.com>
2023-04-29 23:01:01 +0400

    T3

---
+++ FT.java
@@ -0,0 +1,29 @@
+public class FT {
+    private double[] timeValues;
+    private int numHarmonics;
+
+    public FT(double[] timeValues, int numHarmonics) {
+        this.timeValues = timeValues;
+        this.numHarmonics = numHarmonics;
+    }
+
+    public double[] getAmplitudes() {
+        int N = timeValues.length;
+        double[] amplitudes = new double[numHarmonics];
+
+        for (int k = 0; k < numHarmonics; k++) {
+            double real = 0;
+            double imag = 0;
+
+            for (int n = 0; n < N; n++) {
+                double angle = 2 * Math.PI * k * n / N;
+                real += timeValues[n] * Math.cos(angle);
+                imag -= timeValues[n] * Math.sin(angle);
+            }
+
+            amplitudes[k] = Math.sqrt(real * real + imag * imag) / N;
+        }
+
+        return amplitudes;
+    }
+}
