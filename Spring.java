90956175e6bb8e26bede30b9c0720084d0aecd0f
Sevada <sevadaiskandaryan@gmail.com>
2023-04-29 20:11:33 +0400

    T1_u

--- Spring.java
+++ Spring.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.*;
+
 public class Spring {
 
     private double k;
@@ -30,4 +32,40 @@ public class Spring {
         return move(t0, t1, dt, x0, v0, 1);
     }
 
+    public double[] move(double t0, double t1, double dt, double x0, double v0, double m){
+        double[] coordinates = getCoordinatesArray(t1, t0, dt);
+
+        double w = Math.sqrt(k/m); // omega = sqrt(k/m) for harmonic motion
+        double a = Math.sqrt(Math.pow((v0/w),2) + Math.pow(x0, 2)); // the amplitute = sqrt((v/w)^2+x^2)
+        double p = Math.atan2(x0, v0/w); // The phase of the motion
+
+        for (int i = 0; i < coordinates.length; i++) {
+            double x = a*sin(w*(t0+i*dt) + p);
+            coordinates[i] = x;
+        }
+
+        return coordinates;
+    }
+
+    //creates empty array for coordinates with size (t1-t0)/dt
+    public double[] getCoordinatesArray(double t1, double t0, double dt){
+        double[] coordinates = new double[(int) Math.ceil((t1-t0)/dt)];
+        return coordinates;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(); 
+        Spring s = new Spring();
+        
+        System.out.println(s.move(10, 1, 3)); 
+    }
 }
+
+
+
+/*  the formalas are obtained from lecture notes
+    and from this sources
+    https://physics.stackexchange.com/questions/456892/finding-the-amplitude-of-a-spring-oscillation-given-initial-position-and-velocit#:~:text=You'll%20need%20to%20know,velocity%20to%20determine%20the%20amplitude.&text=%CF%89%3D2%CF%80T%3D2,the%20mass%20of%20the%20mass.
+    https://courses.lumenlearning.com/suny-osuniversityphysics/chapter/15-1-simple-harmonic-motion/
+
+    */
---
+++ main.java
@@ -0,0 +1,7 @@
+import static java.lang.Math.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println((int) Math.ceil(1.2)); 
+    }
+}
